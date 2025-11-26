3c6bf2b00f9eb4414a44090feed258baf290031d
Sultan <sultanonazi7@example.com>
2025-11-26 09:47:06 +0300

    updated all notes

--- python-package/employee_events/__init__.py
+++ python-package/employee_events/__init__.py
@@ -2,3 +2,5 @@ from .employee import Employee
 from .team import Team
 from .query_base import QueryBase
 from .sql_execution import *
+
+__all__ = ['Employee', 'Team', 'QueryBase']
--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -6,6 +6,7 @@ import pandas as pd
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
 #### YOUR CODE HERE
+db_path = Path(__file__).parent / "employee_events.db"
 
 
 # OPTION 1: MIXIN
@@ -17,6 +18,12 @@ class QueryMixin:
     # and returns the query's result
     # as a pandas dataframe
     #### YOUR CODE HERE
+    def pandas_query(self, sql_query):
+        """Execute query and return results as pandas DataFrame"""
+        connection = connect(db_path)
+        df = pd.read_sql_query(sql_query, connection)
+        connection.close()
+        return df
 
     # Define a method named `query`
     # that receives an sql_query as a string
@@ -24,8 +31,13 @@ class QueryMixin:
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
     #### YOUR CODE HERE
-    
-
+    def query(self, sql_query):
+        """Execute query and return results as list of tuples"""
+        connection = connect(db_path)
+        cursor = connection.cursor()
+        result = cursor.execute(sql_query).fetchall()
+        connection.close()
+        return result
  
  # Leave this code unchanged
 def query(func):
